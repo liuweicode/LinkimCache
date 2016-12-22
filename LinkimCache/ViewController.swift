8df64ff62628d1806d5ff6b5f4e23e570868054d
liuwei <i@liuwei.co>
2016-12-22 17:48:38 +0800

    Initial Commit

---
+++ LinkimCache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkimCache
+//
+//  Created by 刘伟 on 22/12/2016.
+//  Copyright © 2016 上海凌晋信息技术有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
